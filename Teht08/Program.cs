fe8e3b3cbbd638a4a7517cd1ae666889fbfb7ca1
K2970 <K2970@student.jamk.fi>
2017-01-19 15:14:47 +0200

    valmis

---
+++ Teht08/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anna luku > ");
+            int luku1 = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Anna luku > ");
+            int luku2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Anna luku > ");
+            int luku3 = int.Parse(Console.ReadLine());
+
+            {
+
+                if (luku1 > luku2 && luku1 > luku3)
+                    Console.WriteLine("Suurin luku on " + luku1);
+
+                else if (luku2 > luku1 && luku2 > luku3)
+                    Console.WriteLine("Suurin luku on " + luku2);
+
+                else if (luku3 > luku2 && luku3 > luku1)
+                    Console.WriteLine("Suurin luku on " + luku3);
+            }
+        }
+    }
+}
